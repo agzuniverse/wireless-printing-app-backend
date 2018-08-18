4cb91201643c224ed51ffeee2e024c9659832bba
Aswin G <aswinganesh666@gmail.com>
2018-08-18 17:39:45 +0530

    Create database table "UserData"
    Table currently hold credits. Can hold other data such as history in
    the future.

--- backend/wireless_printing/models.py
+++ backend/wireless_printing/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Test(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField()
+
+
+class UserData(models.Model):
+    id = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+    credits = models.IntegerField(default=0)
