9df45cff6bcb19b212c826d2059484f11775da58
Vivek R <123vivekr@gmail.com>
2019-07-23 14:50:57 +0530

    Update tasks.py

--- backend/wireless_printing/tasks.py
+++ backend/wireless_printing/tasks.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import, unicode_literals
-from celery import shared_task
+# from celery import shared_task
 from .util import remove_credits
 import os
 
-@shared_task
+# @shared_task
 def print_file(path, from_page, to_page, color, credits, user):
     # SEND FILE TO PRINTER FOR PRINTING
     os.system("lpr " + "-o page-ranges=1-" + to-page + " " + path)
